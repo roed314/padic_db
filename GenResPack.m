4ed3dc614712b6e86c5d0ccaa93e27ee771eec17
Sebastian Pauli <s_pauli@uncg.edu>
2026-06-01 11:45:08 -0400

    moved indeces of inseperarbility functions from AllExtensions to GenResPack

--- GenResPack.m
+++ GenResPack.m
@@ -1,3 +1,16 @@
+/*
+Copyright 2025 by Kevin Keating
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see
+ <https://www.gnu.org/licenses/>.
+*/
+
+
+
 intrinsic Indices(f::RngUPolElt) -> []
 {This computes the indices of inseparability of the extension L/K,
 where L is generated over K by a root of f, which is an Eisenstein
@@ -113,3 +126,73 @@ FqAz.}
    end if;
    return respolys;
 end intrinsic;
+
+
+
+///////////////////////////////
+// Indices of inseparability
+
+intrinsic AllIndicesOfInseperability(K,n,j) -> .
+{Input:  Output: }
+  js := PossibleDiscriminants(K,n);
+  if not j in js then error "j+n-1 =",j,"+",n,"- 1 is not a valid discriminant exponent.  Valid values for j are",js; end if;
+  p := Prime(K);
+  nu := Valuation(n,p);
+  ek := RamificationIndex(K);
+  Is := [[0]];
+  k := nu;
+  while k gt 1 do
+    Isk := [];
+    for this_i in Is do
+      if Valuation(this_i[#this_i],p) lt k then
+        Append(~Isk, this_i cat [this_i[#this_i]]);
+      else
+        for ij1 in [this_i[#this_i]..Minimum(this_i[#this_i]+n*ek,j)] do
+          if ij1 ge j-Valuation(ij1,p)*n*ek then 
+            if ij1 eq this_i[#this_i] + n*ek then
+              Append(~Isk, this_i cat [ij1]);  
+            elif 1 le Valuation(ij1,p) and Valuation(ij1,p) le k-1 then
+              Append(~Isk, this_i cat [ij1]);
+            elif ij1 eq j then
+              Append(~Isk, this_i cat [ij1]);
+            end if;
+          end if;
+        end for;
+      end if;
+      Is := Isk;
+    end for;
+    k := k-1;
+  end while;
+  Isr := [];
+  for this_i in Is do
+    new_i := this_i cat [j];
+    if (Valuation(new_i[#new_i],p) ge 1 and new_i[#new_i] eq new_i[#new_i-1]+n*ek) or Valuation(new_i[#new_i],p) eq 0 then
+      Append(~Isr,Reverse(new_i));
+    end if;
+  end for;
+  return Isr;
+end intrinsic;
+
+
+intrinsic IndicesOfInseperability(f) -> .
+{Input: f - an Eisenstein polynomial (correctness of input is not checked).  Output: List of inices of Inseperability}
+
+  n := Degree(f);
+  K := CoefficientRing(f);
+  nu := Valuation(K!n);
+  p := Prime(K);
+  i_0 := Valuation(Discriminant(f))-n+1;
+  itilde := [];
+  for j in [0..nu] do
+    Append(~itilde, Minimum([n*Valuation(Coefficient(f,i))+i-n : i in [1..n] | Valuation(K!i) le j]));
+  end for;
+  i := [-1: k in [0..nu]];
+  i[nu+1] := 0;
+  for j in [nu-1..0 by -1] do
+    i[j+1] := Minimum(itilde[j+1],i[j+2]+n*Valuation(K!p));
+  end for;
+  return i;
+end intrinsic;
+
+
+
